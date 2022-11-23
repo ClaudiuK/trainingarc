18c3233330bd3576bd52a735fcda5979a66a3d53
kondrei claudiu <xclaudiuxd@gmail.com>
2022-11-23 09:31:58 +0200

    Add project files.

---
+++ trainingarc/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace trainingarc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ex 1.V of rectangular parallelepiped.
+            int L;//Big lenght
+            int l;//small lenght
+            int h;// height
+            Console.WriteLine("Introduceti lungimea mare :");
+            L = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti lungimea mica :");
+            l = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti naltimea :");
+            h = int.Parse(Console.ReadLine());         
+
+
+
+             int Volume = L * l * h;
+            Console.WriteLine("Volumul paralelipipedului este:  " + Volume);
+
+        }
+    }
+}
